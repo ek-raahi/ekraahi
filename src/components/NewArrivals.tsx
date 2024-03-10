31a68206cb49e611771df6c074225ae31e5b0e4c
divyanshu <divyanshulohar2002@gmail.com>
2024-03-10 18:22:10 +0530

    some changes

--- src/components/NewArrivals.tsx
+++ src/components/NewArrivals.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const NewArrivals = () => {
@@ -7,13 +8,14 @@ const NewArrivals = () => {
             <h1>New Arrivals</h1>
         </div>
         <div className='h-[82vh] w-[80vw] flex '>
-            <div className='w-[28%] h-full border-2 border-white relative'>
+            
+            <Link href={"/product"} className='w-[28%] h-full border-2 border-white relative'>
                 <div className='absolute bottom-0 text-white flex flex-col justify-center pl-[2vw] text-2xl leading-relaxed h-[14vh] w-full fade z-10'>
                     <h2>Sheer Pullover T-Shirt</h2>
                     <h2 className='font-semibold'>$12.89</h2>
                 </div>
                 <img src={'/cat6.jpg'} alt="cat1" className='object-cover h-full'/>
-            </div>
+            </Link>
             <div className='w-[72%] h-full ml-[1.5vw] grid grid-cols-3 gap-4 content-start'>
                 <div className='w-[18vw] h-[40vh] border-2 border-white relative'>
                     <div className='absolute bottom-0 text-white flex flex-col justify-center pl-[2vw] text-xl leading-relaxed h-[12vh] w-full fade z-10'>
